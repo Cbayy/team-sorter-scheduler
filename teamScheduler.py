b0f8ea5023645ee99d679d6bc72df9c56be5ff87
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-12-23 21:27:47 +1030

    Added calculation to determine number of games played in round robin.

--- teamScheduler.py
+++ teamScheduler.py
@@ -1,2 +1,12 @@
-def schedule():
-    print("Ran")
+def schedule(groupNum):
+    courtNum = getCourtNum()
+    createGames(groupNum, courtNum)
+    print("Scheduler finished")
+
+def getCourtNum():
+    courtNum = input("Enter the number of courts: ")
+    return courtNum
+
+def createGames(groupNum, courtNum):
+    arr = [[0]*2]*int(((groupNum*(groupNum-1))/2))
+    print(len(arr))
